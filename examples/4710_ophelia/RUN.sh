a5983e24a8b272a014afe8d241606571e1f74ff9
Linton Corbet <linton.corbet@chmi.cz>
2013-06-12 11:27:04 +0000

    As we are in examples, no need to have sample files.
    All files are 'samples', so keep only files that
    might be useful as examples (INFO_RUN.nml.sample,
    because this file is usually written by the WRITE.. script).

--- examples/4710_ophelia/RUN.sh
+++ examples/4710_ophelia/RUN.sh
@@ -4,8 +4,9 @@ aladin2camx="/home/linton/work/aladin2camx/aladin2camx/aladin2camx_MAIN.exe"
 curdir="/home/linton/work/aladin2camx/aladin2camx/examples/4710_ophelia"
 
 SD=`date --utc --date yesterday +%Y-%m-%d` #"2013-06-06"
-SH=1
-NH=6
+SD="2013-06-06"
+SH=13 # SH must point to a startStep=1 file, so that we use startStep=0 for file 0.
+NH=5 # in the case of hourly fields, the resulting file will have NH+1 fields.
 NG=2
 GRIBDIR="${curdir}/TMPGRIB"
 CAMXDIR="${curdir}/CAMXINP"
@@ -23,14 +24,15 @@ echo "$NML written."
 # Ensure the gribs are in position
 rm -f ${GRIBDIR}/*
 i=0
+hrint=`date -u -d "$SD +$[$SH+$i-1]hours" +%-H`
+aladrun=`printf "%02d" $[($hrint)/6*6]`            #aladin run id (00, 06, 12 or 18)
 while [ $i -le $[$NH+1] ] ; do
-  rawstamp=`date -u -d "$SD +$[$SH+$i-1]hours" +%Y%m%d`  #the date of the data
-  hr=`date -u -d "$SD +$[$SH+$i-1]hours" +%H`            #the hour of the data
   hrint=`date -u -d "$SD +$[$SH+$i-1]hours" +%-H`
-
-  aladrun=`printf "%02d" $[($hrint-1)/6*6]`            #aladin run id (00, 06, 12 or 18)
+  hr=`printf "%02d" $hrint`
   step=`printf "%02d" $[$hrint - $aladrun]`        #timestep starting from that run id
+  echo ${aladrun}_${step}
   rawgribdir="/mnt/storage1/aq/data/aladin/operativa/current"
+  rawstamp=`date -u -d "$SD +$[$SH+$i-1]hours" +%Y%m%d`
   rawgrib="${rawgribdir}/ALAD4camx_${rawstamp}${aladrun}_${step}.grb"
   ln -s ${rawgrib} ${GRIBDIR}/ALAD4camx_${rawstamp}_${hr}.grb
   i=$[$i + 1]
