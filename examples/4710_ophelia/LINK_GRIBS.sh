2a447fe8ae8b0ed94fb555aa5381704dc64af876
Linton Corbet <linton.corbet@chmi.cz>
2013-06-12 13:02:06 +0000

    Link 6-hourly blocks of aladin forecasts from step=01-06, not using 00.

--- examples/4710_ophelia/LINK_GRIBS.sh
+++ examples/4710_ophelia/LINK_GRIBS.sh
@@ -15,18 +15,22 @@ aladrun=`printf "%02d" $[($f0_HHint)/6*6]`
 step=`printf "%02d" $[$f0_HHint - $aladrun]`
 rawgrib="${FROMDIR}/ALAD4camx_${f0_YYYYMMDD}${aladrun}_${step}.grb"
 linkedgrib="${TODIR}/ALAD4camx_${f0_YYYYMMDD}_${f0_HH}.grb"
-echo ln -sv $rawgrib $linkedgrib
+ln -sv $rawgrib $linkedgrib
 
 
 # Remaining Hours
+i=1
 while [ $i -le $[$NH] ] ; do
+  rundt=`date -u -d "$SD +$[$SH+$i-1]hours" +%Y%m%d` #the real date!
   hrint=`date -u -d "$SD +$[$SH+$i-1]hours" +%-H`
-  aladrun=`printf "%02d" $[($hrint-1)/6*6]`
+  rundtm1=`date -u -d "$SD +$[$SH+$i-2]hours" +%Y%m%d` #one hour less that real date!
+  hrintm1=`date -u -d "$SD +$[$SH+$i-2]hours" +%-H`
+  runid=`printf "%02d" $[($hrintm1)/6*6]`
+  step=`printf "%02d" $[$hrintm1 +1 - $runid]`
   hr=`printf "%02d" $hrint`
-  step=`printf "%02d" $[$hrint - $aladrun]`
-  rawgrib="${FROMDIR}/ALAD4camx_${f0_YYYYMMDD}${aladrun}_${step}.grb"
-  linkedgrib="${TODIR}/ALAD4camx_${f0_YYYYMMDD}_${hr}.grb"
-  echo ln -sv $rawgrib $linkedgrib
+  rawgrib="${FROMDIR}/ALAD4camx_${rundtm1}${runid}_${step}.grb"
+  linkedgrib="${TODIR}/ALAD4camx_${rundt}_${hr}.grb"
+  ln -sv $rawgrib $linkedgrib 
   i=$[$i+1]
 
 
