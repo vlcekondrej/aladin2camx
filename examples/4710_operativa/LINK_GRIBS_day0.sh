5df91b36a185cfda6077309300e79d58588c9296
Linton Corbet <linton.corbet@chmi.cz>
2013-06-14 10:43:37 +0000

    name change

--- examples/4710_operativa/LINK_GRIBS_day0.sh
+++ examples/4710_operativa/LINK_GRIBS_day0.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 # Inputs
-FROMDIR=$1
-TODIR=$2
-SD=$3 # YYYY-MM-DD
-SH=$4 #start hour
-NH=$5 #number of hours
+today=`date -u -d "today" +%Y-%m-%d`
+FROMDIR=${1:-"."}
+TODIR=${2:-"."}
+SD=${3:-today} # YYYY-MM-DD
+SH=${4:-0} #start hour
+NH=${5:-25} #number of hours
 
 # The 0th hour file (hidden from user, used for solar radiation calculation)
 f0_YYYYMMDD=`date -u -d "$SD +$[$SH-1]hours" +%Y%m%d`
@@ -15,11 +16,12 @@ aladrun=`printf "%02d" $[($f0_HHint)/6*6]`
 step=`printf "%02d" $[$f0_HHint - $aladrun]`
 rawgrib="${FROMDIR}/ALAD4camx_${f0_YYYYMMDD}${aladrun}_${step}.grb"
 linkedgrib="${TODIR}/ALAD4camx_${f0_YYYYMMDD}_${f0_HH}.grb"
-ln -sv $rawgrib $linkedgrib
+echo ln -sv $rawgrib $linkedgrib
 
 
-# Remaining Hours
+# Remaining Hours ! not cycling on 6-hr intervals but running straight through
 i=1
+grbdt=`date -u -d "$SD +$[$SH+$i-1]hours" +%Y%m%d` #the real date!
 while [ $i -le $[$NH] ] ; do
   rundt=`date -u -d "$SD +$[$SH+$i-1]hours" +%Y%m%d` #the real date!
   hrint=`date -u -d "$SD +$[$SH+$i-1]hours" +%-H`
@@ -30,7 +32,7 @@ while [ $i -le $[$NH] ] ; do
   hr=`printf "%02d" $hrint`
   rawgrib="${FROMDIR}/ALAD4camx_${rundtm1}${runid}_${step}.grb"
   linkedgrib="${TODIR}/ALAD4camx_${rundt}_${hr}.grb"
-  ln -sv $rawgrib $linkedgrib 
+  echo ln -sv $rawgrib $linkedgrib 
   i=$[$i+1]
 
 
