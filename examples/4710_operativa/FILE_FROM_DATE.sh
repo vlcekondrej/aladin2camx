aec287811a3ae3bf392f19c8d20301c11fb40d71
Linton Corbet <linton.corbet@chmi.cz>
2013-06-14 12:27:25 +0000

    To create aladin or camx filenames

---
+++ examples/4710_operativa/FILE_FROM_DATE.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+# source "this file"
+# then use the functions
+
+
+aladinGribName_00fc () {
+    # for hours -1 and 0, we use the previous forecast's fields.
+    date=$1 # YYYY-MM-DD format
+    hour=$2 # integer value from 00UTC, can be 48 if you want that hour's forecast.
+    case $hour in
+      "-1") 
+           grbdt=`date -u -d "$date -1hour" +%Y%m%d`
+	   fname="ALAD4camx_${grbdt}18_05.grb"
+	   ;;
+       "0")  
+           grbdt=`date -u -d "$date -1hour" +%Y%m%d` 
+           fname="ALAD4camx_${grbdt}18_06.grb"
+           ;;
+       *)
+           grbdt=`date -u -d "$date" +%Y%m%d`
+    	   step=`printf "%02d" $hour`
+           fname="ALAD4camx_${grbdt}00_${step}.grb"
+           ;;
+    esac
+    echo "${fname}"
+}
+
+aladin2camxGribName () {
+    #ALAD4camx_${f0_YYYYMMDD}_${f0_HH}.grb"
+    date=$1 # YYYY-MM-DD format
+    hour=$2 # integer value from 00UTC
+    dt=`date -u -d "$date +${hour}hours" +%Y%m%d_%H`
+    echo "ALAD4camx_${dt}.grb"
+}
+
+
+aladinGribName_cont () {
+    #continuous use of updated 6hrly forecasts
+    #$1 # YYYY-MM-DD format
+    #$2 # integer value from 00UTC     
+    date=`date -u -d "$1 +${2}hours" +%Y-%m-%d`
+    hour=`date -u -d "$1 +${2}hours" +%-H`
+    step=$[${hour}%6] 
+    if [ $step -eq 0 ] ; then
+      prevdt=`date -u -d "$date +$[${hour}-1]hours" +%Y%m%d`
+      prevhr=`date -u -d "$date +$[${hour}-1]hours" +%-H`
+      aladrun=`printf "%02d" $[(${prevhr})/6*6]`
+      aladstep=`printf "%02d" $[$prevhr - $aladrun +1]`
+      fname=`printf "ALAD4camx_%8d%02d_%02d.grb" ${prevdt} $aladrun $aladstep`
+    else
+      dt=`date -u -d "$date +${hour}hours" +%Y%m%d`
+      run=`printf "%02d" $[($hour)/6*6]`
+      fname=`printf "ALAD4camx_%8d%02d_%02d.grb" $dt $run $step`
+    fi
+    echo $fname
+}
+
+
